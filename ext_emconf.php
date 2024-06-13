4db6706b2cd82340a53c60425d896b90b8abcdd3
A. wolo Wolski <wolo.wolski@gmail.com>
2024-06-13 09:11:22 +0200

    fix constraints to scope for Tailor requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -28,7 +28,7 @@ this extension is very useful. It\'s simple and easy to configure, but very flex
 	'author_company' => 'wolo.pl \'.\' studio, Q3i',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '12.4.99',
+			'typo3' => '12.4.0-12.4.99',
 		],
 		'conflicts' => [
 		],
